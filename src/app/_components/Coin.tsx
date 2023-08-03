8712826fd58ced2d360d718b2a11f8bef58f448c
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-03 21:52:17 +0200

    using useSWR to fetch eth price

--- src/app/_components/Coin.tsx
+++ src/app/_components/Coin.tsx
@@ -4,60 +4,58 @@
 
 import React, { useState, useEffect } from 'react';
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import useSWR from 'swr';
 interface EthPriceData {
   [currency: string]: number;
 }
 const axiosInstance = axios.create({
   baseURL: 'https://api.coingecko.com/api/v3/simple/',
 });
-
+const getPrice = async (currency: string) => {
+  const params = {
+    ids: 'ethereum',
+    vs_currencies: currency,
+  };
+  const response: AxiosResponse<EthPriceData> = await axiosInstance.get<EthPriceData>("/price", {
+    params: params,
+  });
+  const ethPriceData: EthPriceData = response.data;
+  return ethPriceData;
+}
 const Coin = () => {
   const [coinPrice, setCoinPrice] = useState<EthPriceData>();
   const [currency, setCurrency] = useState<string>("usd");
-  useEffect(() => {
-    const fetchPrice = async () => {
-      const params = {
-        ids: 'ethereum',
-        vs_currencies: currency,
-      };
-      try {
-        const response: AxiosResponse<EthPriceData> = await axiosInstance.get<EthPriceData>("/price", {
-          params: params,
-        });
-        const ethPriceData: EthPriceData = response.data;
-        setCoinPrice(ethPriceData)
-      } catch (error) {
-        console.log(error);
-      }
-      finally {
-        console.log("finally");
-      }
-
-    }
-    fetchPrice();
-  }, [currency]);
+  const key = '/price?ids=ethereum&vs_currencies=' + currency; // Define the cache key
+  const { data, error } = useSWR(key, () => getPrice(currency));
+  console.log(data);
+  console.log(error);
+  // useEffect(() => {
+  //   const fetchPrice = async () => {
+  //     const params = {
+  //       ids: 'ethereum',
+  //       vs_currencies: currency,
+  //     };
+  //     try {
+  //       const response: AxiosResponse<EthPriceData> = await axiosInstance.get<EthPriceData>("/price", {
+  //         params: params,
+  //       });
+  //       const ethPriceData: EthPriceData = response.data;
+  //       setCoinPrice(ethPriceData)
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //     finally {
+  //       console.log("finally");
+  //     }
+  //
+  //   }
+  //   fetchPrice();
+  // }, [currency]);
   return (<div>
     <h3>Coin</h3>
-    {/* <table> */}
-    {/*   <thead> */}
-    {/*     <tr> */}
-    {/*       <th>Currency</th> */}
-    {/*       <th>Price</th> */}
-    {/*     </tr> */}
-    {/*   </thead> */}
-    {/*   <tbody> */}
-    {/*     {coinPrice && Object.keys(coinPrice).map((currency: string) => { */}
-    {/*       return ( */}
-    {/*         <tr key={currency}> */}
-    {/*           <td>{currency}</td> */}
-    {/*           <td>{coinPrice[currency]}</td> */}
-    {/*         </tr> */}
-    {/*       ) */}
-    {/*     })} */}
-    {/*   </tbody> */}
-    {/* </table> */}
     <div>
-      {coinPrice && JSON.stringify(coinPrice)}
+      {/* {coinPrice && JSON.stringify(coinPrice)} */}
+      {data && JSON.stringify(data)}
     </div>
   </div>);
 }
