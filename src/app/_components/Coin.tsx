d7ffffe4819547d93e3506aeff241e89de6279fa
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-03 22:43:58 +0200

    updating useSWR fetch method

--- src/app/_components/Coin.tsx
+++ src/app/_components/Coin.tsx
@@ -2,59 +2,40 @@
 // this component will fetch coingecko api to get the ethereum price
 // and display it in a table
 
-import React, { useState, useEffect } from 'react';
+import React, { useMemo, useState } from 'react';
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import useSWR from 'swr';
 interface EthPriceData {
   [currency: string]: number;
 }
-const axiosInstance = axios.create({
+const axiosInstance: AxiosInstance = axios.create({
   baseURL: 'https://api.coingecko.com/api/v3/simple/',
 });
-const getPrice = async (currency: string) => {
-  const params = {
-    ids: 'ethereum',
-    vs_currencies: currency,
-  };
-  const response: AxiosResponse<EthPriceData> = await axiosInstance.get<EthPriceData>("/price", {
-    params: params,
+type GetPrice = (args: string[]) => Promise<EthPriceData>;
+const getPrice: GetPrice = async (args) => {
+  console.log(args);
+  const response: AxiosResponse<EthPriceData> = await axiosInstance.get<EthPriceData>(args[0], {
+    params: args[1],
   });
   const ethPriceData: EthPriceData = response.data;
   return ethPriceData;
 }
 const Coin = () => {
-  const [coinPrice, setCoinPrice] = useState<EthPriceData>();
+  // const [coinPrice, setCoinPrice] = useState<EthPriceData>();
   const [currency, setCurrency] = useState<string>("usd");
-  const key = '/price?ids=ethereum&vs_currencies=' + currency; // Define the cache key
-  const { data, error } = useSWR(key, () => getPrice(currency));
-  console.log(data);
-  console.log(error);
-  // useEffect(() => {
-  //   const fetchPrice = async () => {
-  //     const params = {
-  //       ids: 'ethereum',
-  //       vs_currencies: currency,
-  //     };
-  //     try {
-  //       const response: AxiosResponse<EthPriceData> = await axiosInstance.get<EthPriceData>("/price", {
-  //         params: params,
-  //       });
-  //       const ethPriceData: EthPriceData = response.data;
-  //       setCoinPrice(ethPriceData)
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //     finally {
-  //       console.log("finally");
-  //     }
-  //
-  //   }
-  //   fetchPrice();
-  // }, [currency]);
+  const params = useMemo(() => {
+    return {
+      ids: 'ethereum',
+      vs_currencies: currency,
+    };
+  }, [currency]);
+
+  const { data, error, mutate, isLoading } = useSWR(["/price", params], getPrice);
   return (<div>
     <h3>Coin</h3>
     <div>
       {/* {coinPrice && JSON.stringify(coinPrice)} */}
+      {isLoading && <div>Loading...</div>}
       {data && JSON.stringify(data)}
     </div>
   </div>);
