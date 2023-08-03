01786d21dc05de27d340e04c5e7c306c7a57db78
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-04 01:21:27 +0200

    updated coin to accept props string or number

--- src/app/_components/Coin.tsx
+++ src/app/_components/Coin.tsx
@@ -14,7 +14,7 @@ const axiosInstance: AxiosInstance = axios.create({
 type GetPriceArg = [string, { ids: string, vs_currencies: string }];
 type GetPrice = (args: GetPriceArg) => Promise<EthPriceData>;
 const getPrice: GetPrice = async (args) => {
-  // console.log(args);
+  console.log(args);
   const response: AxiosResponse<EthPriceData> = await axiosInstance.get<EthPriceData>(args[0], {
     params: args[1],
   });
@@ -22,17 +22,16 @@ const getPrice: GetPrice = async (args) => {
   return ethPriceData;
 }
 
-const Coin = ({ ethValue }: { ethValue: number }) => {
-  // const [coinPrice, setCoinPrice] = useState<EthPriceData>();
-  const [currency, setCurrency] = useState<string>("usd");
-  const params = useMemo(() => {
-    return {
-      ids: 'ethereum',
-      vs_currencies: currency,
-    };
-  }, [currency]);
-
-  const { data, error, mutate, isLoading } = useSWR(["/price", params], getPrice, {
+const Coin = ({ ethValue }: { ethValue: string | number }) => {
+  let ethValueToNumber = typeof ethValue === 'string' ? parseFloat(ethValue) : ethValue;
+  if (Number.isNaN(ethValueToNumber)) {
+    ethValueToNumber = 0;
+  }
+  const params = {
+    ids: 'ethereum',
+    vs_currencies: 'usd',
+  }
+  const { data, error, mutate, isValidating } = useSWR(["/price", params], getPrice, {
     errorRetryInterval: 60 * 1000,
     errorRetryCount: 3,
   }
@@ -41,21 +40,45 @@ const Coin = ({ ethValue }: { ethValue: number }) => {
   if (error) console.error(error)
   let priceUsd;
   if (data) {
-    console.log("data")
-    console.log(data.ethereum.usd)
-    console.log(ethValue)
-    priceUsd = data.ethereum.usd * ethValue;
-
+    priceUsd = data.ethereum.usd * ethValueToNumber;
   }
-  console.log("priceUsd", priceUsd)
-  return (<div>
-    <h3>Coin</h3>
-    <div>
-      {/* <button onClick={() => setCurrency("usd")}>USD</button> */}
-      {priceUsd && priceUsd > 0 ? <div>{priceUsd}</div> : '0 $'}
-      {error && <div>Failed to fetched price</div>}
-      {isLoading && <div>Loading...</div>}
-    </div>
-  </div>);
-}
+
+  const handleRefresh = () => {
+    mutate();
+  };
+  return (<div className="container mx-auto mt-8 max-w-md">
+    <h3 className="text-xl font-semibold mb-4">Coin Price</h3>
+    {isValidating && <div className="mb-2">Loading...</div>}
+    {!isValidating && (
+      <div className="flex flex-col">
+        {data ? (
+          <div className="mb-2">Price: {priceUsd} $</div>
+        ) : (
+          <div className="mb-2">Price: N/A</div>
+        )}
+        {error && (
+          <div className="text-red-500 mb-2">Error fetching data</div>
+        )}
+        <div>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            onClick={handleRefresh}
+          >
+            Refresh
+          </button>
+          {error && (
+            <button
+              className="bg-red-500 text-white px-4 py-2 rounded ml-2"
+              onClick={handleRefresh}
+            >
+              Retry
+            </button>
+          )}
+        </div>
+      </div>
+    )}
+  </div>
+  );
+};
+
 export default Coin;
