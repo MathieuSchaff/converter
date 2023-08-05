ba95a30c6707371d0c5bcdb79b895e4ca9740e24
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-05 14:26:50 +0200

    coins page init

---
+++ src/app/coins/page.tsx
@@ -0,0 +1,8 @@
+import { PageWrapper } from "../_components/PageWrapper"
+const CoinsPage = () => {
+  return (
+    <PageWrapper className="bg-red-800">
+      <div className="">Coin page</div>
+    </PageWrapper>)
+}
+export default CoinsPage
